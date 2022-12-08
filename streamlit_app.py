da33952a567635bec3bd0e3803939fb83b6adfef
dxmai <19521820@gm.uit.edu.vn>
2022-12-08 23:00:29 +0700

    add event model

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,29 @@ def install():
 
 # install()
 
+# ====================== Load additional ======================
+def load():
+    model_path = os.getcwd() + '/model/knn_insight.pickle'
+    with open(model_path, "rb") as file:
+        clf = pickle.load(file)
+    dict_path = os.getcwd() + '/model/dict_insight.pickle'
+    with open(dict_path, "rb") as file:
+        dic = pickle.load(file)
+    event_path = os.getcwd() + '/even_info/event_dict.pickle'
+    with open(event_path, "rb") as file:
+        event_dict = pickle.load(file)
+    info_path = os.getcwd() + '/even_info/event_info.pickle'
+    with open(info_path, "rb") as file:
+        event_info = pickle.load(file)
+    event_model_p = os.getcwd() + '/model/knn_event.pickle'
+    with open(event_model_p, "rb") as file:
+        event_clf = pickle.load(file)
+    return clf, dic, event_dict, event_info, event_clf
+
+clf, dic, event_dict, event_info, event_clf = load()
+
+path = os.getcwd() 
+
 # ====================== Get input image ======================
 st.sidebar.write(":open_file_folder: Tải ảnh lên")
 uploaded_image = st.sidebar.file_uploader("uploader", label_visibility="collapsed")
@@ -50,50 +73,36 @@ if show:
     st.sidebar.write("Ảnh đã chọn")
     st.sidebar.image(img)
 
-# ====================== Load additional ======================
-def load():
-    model_path = os.getcwd() + '/model/knn_insight.pickle'
-    with open(model_path, "rb") as file:
-        clf = pickle.load(file)
-    dict_path = os.getcwd() + '/model/dict_insight.pickle'
-    with open(dict_path, "rb") as file:
-        dic = pickle.load(file)
-    return clf, dic
-
-clf, dic = load()
-
-path = os.getcwd() 
-
 # ====================== Run model ======================
 run = st.sidebar.button("Dự đoán")
 if run and img != '': 
     with st.spinner("Vui lòng chờ một chút..."):
         # ====================== Face Dectection and Recognition ======================
-        # res_face, embeddings = detect_face_ins(img)
-        # fig = plt.figure(figsize = (5,5))
-        # ax = fig.add_axes([0, 0, 1, 1])
-        # predicted = []
-        # for embedding in embeddings:
-        #     embedding = embedding.reshape(-1, 512)
-        #     name = clf.predict(embedding)
-        #     predicted.append(dic[name[0]])
-        # labels = draw_boundingbox(ax, res_face, predicted)
+        res_face, embeddings = detect_face_ins(img)
+        fig = plt.figure(figsize = (5,5))
+        ax = fig.add_axes([0, 0, 1, 1])
+        predicted = []
+        for embedding in embeddings:
+            embedding = embedding.reshape(-1, 512)
+            name = clf.predict(embedding)
+            predicted.append(dic[name[0]])
+        labels = draw_boundingbox(ax, res_face, predicted)
 
         # ====================== Matting ======================
         # Save image to get matting input
-        source_img = 'query.jpg'
-        cv2.imwrite(source_img, cv2.cvtColor(img, cv2.COLOR_RGB2BGR)) 
+        # source_img = 'query.jpg'
+        # cv2.imwrite(source_img, cv2.cvtColor(img, cv2.COLOR_RGB2BGR)) 
         # Matting result image
-        des_img = path + "/result/matting.jpg"
+        # des_img = path + "/result/matting.jpg"
 
-        subprocess.run(["python", "download_inference_models.py"])
-        subprocess.run(["python", "download_data.py"])
-        subprocess.run(["python", "seg_demo.py", "--config", "inference_models/portrait_pp_humansegv2_lite_256x144_inference_model_with_softmax/deploy.yaml", "--img_path", source_img, "--save_dir", des_img])
+        # subprocess.run(["python", "download_inference_models.py"])
+        # subprocess.run(["python", "download_data.py"])
+        # subprocess.run(["python", "seg_demo.py", "--config", "inference_models/portrait_pp_humansegv2_lite_256x144_inference_model_with_softmax/deploy.yaml", "--img_path", source_img, "--save_dir", des_img])
     
-        # Get the background
-        res = Image.open(des_img)
-        res = np.array(res)
-        background = np.where(res==254, img, 0)
+        # # Get the background
+        # res = Image.open(des_img)
+        # res = np.array(res)
+        # background = np.where(res==254, img, 0)
         
         # ====================== Event Classification ======================
 
@@ -102,39 +111,41 @@ if run and img != '':
         histogram_region = get_region_histogram(img)
         st.write(histogram_region.shape)
 
-        # MobileNet
-        mobile_net = get_mobilenet(img)
-        mobile_net = mobile_net.reshape(mobile_net.shape[0], mobile_net.shape[1] * mobile_net.shape[2] * mobile_net.shape[3])
-        st.write(mobile_net.shape)
+        # # MobileNet
+        # mobile_net = get_mobilenet(img)
+        # mobile_net = mobile_net.reshape(mobile_net.shape[0], mobile_net.shape[1] * mobile_net.shape[2] * mobile_net.shape[3])
+        # st.write(mobile_net.shape)
 
-        # ===== Extract background image =====
-        # Hisrogram RGB
-        hist_rgb = CalHistogram_RGB(background)
-        st.write(hist_rgb.shape)
+        # # ===== Extract background image =====
+        # # Hisrogram RGB
+        # hist_rgb = CalHistogram_RGB(background)
+        # st.write(hist_rgb.shape)
 
-        # Dominant color
-        number_of_colors = 5
-        dominant_color = get_dominant_color(img, 5)
-        dominant_color = check_color(dominant_color)
-        st.write(dominant_color.shape)
+        # # Dominant color
+        # number_of_colors = 5
+        # dominant_color = get_dominant_color(img, 5)
+        # dominant_color = check_color(dominant_color)
+        # st.write(dominant_color.shape)
         
-        # Concatenate
-        event_feature = np.concatenate((histogram_region, mobile_net, hist_rgb, dominant_color), axis=None)
-        event_feature = event_feature.reshape(1, -1)
-        st.write(event_feature.shape)
+        # # Concatenate
+        # event_feature = np.concatenate((histogram_region, mobile_net, hist_rgb, dominant_color), axis=None)
+        # event_feature = event_feature.reshape(1, -1)
+        # st.write(event_feature.shape)
 
         # ===== Classifier =====
-
+        event_label = event_clf.predict(histogram_region)
+        info = event_info[int(event_label)]
 
 
     st.subheader("Kết quả")
-    st.image(background)
-    # string = ["{}: {}".format(key, value) for key, value in zip(labels.keys(), labels.values())]
-    # string = "; ".join(string)
-    # st.write(":adult:", string)
-    # plt.imshow(img)
-    # plt.axis('off')
-    # st.pyplot(fig)
+    string = ["{}: {}".format(key, value) for key, value in zip(labels.keys(), labels.values())]
+    string = "; ".join(string)
+    st.write(":adult:", string)
+    st.write(":date:", info['event_date'])
+    st.write(":ballot_box_with_check:", info['event_name'])
+    plt.imshow(img)
+    plt.axis('off')
+    st.pyplot(fig)
 
 # ====================== Sample Part ======================
 st.subheader("Một vài sự kiện mẫu")
