08b72adfef46158c4d74f9cdc6215fb9e8c41b49
dxmai <19521820@gm.uit.edu.vn>
2023-03-05 23:26:58 +0700

    fix read color chanel

--- feature_extractor.py
+++ feature_extractor.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 from img2vec_pytorch import Img2Vec
 
 def CalHistogram(img, bin=[8,8,8]):
-    img = img[:,:,:3]
+    img = img[:, :, ::-1]
+    img = np.array(img)
     hist = cv2.calcHist([img],[0, 1, 2],None,[bin[0], bin[1], bin[2]],[0,256, 0, 256, 0, 256])
     hist = hist.reshape(1, -1)/ hist.sum()
     return hist
